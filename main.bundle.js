302e3fc5bdb533c7c1423c6eb529f39b8161841b
Leo Bruell <leobruell@gmail.com>
2022-12-07 14:25:47 -0500

    Set up project files

---
+++ main.bundle.js
@@ -0,0 +1,26 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('testifsndal sd')
+// function component() {
+//     const element = document.createElement('div');
+  
+//     const btn = document.createElement('button');
+  
+  
+//     element.innerHTML = "'Hello', 'webpack'"
+//     element.classList.add('hello');
+  
+//     btn.innerHTML = 'Click me and check the console!';
+  
+//     element.appendChild(btn);
+  
+//     return element;
+//   }
+  
+//   document.body.appendChild(component());
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EsNEMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZXN0YXVyYW50LXBhZ2UvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ3Rlc3RpZnNuZGFsIHNkJylcbi8vIGZ1bmN0aW9uIGNvbXBvbmVudCgpIHtcbi8vICAgICBjb25zdCBlbGVtZW50ID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgnZGl2Jyk7XG4gIFxuLy8gICAgIGNvbnN0IGJ0biA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ2J1dHRvbicpO1xuICBcbiAgXG4vLyAgICAgZWxlbWVudC5pbm5lckhUTUwgPSBcIidIZWxsbycsICd3ZWJwYWNrJ1wiXG4vLyAgICAgZWxlbWVudC5jbGFzc0xpc3QuYWRkKCdoZWxsbycpO1xuICBcbi8vICAgICBidG4uaW5uZXJIVE1MID0gJ0NsaWNrIG1lIGFuZCBjaGVjayB0aGUgY29uc29sZSEnO1xuICBcbi8vICAgICBlbGVtZW50LmFwcGVuZENoaWxkKGJ0bik7XG4gIFxuLy8gICAgIHJldHVybiBlbGVtZW50O1xuLy8gICB9XG4gIFxuLy8gICBkb2N1bWVudC5ib2R5LmFwcGVuZENoaWxkKGNvbXBvbmVudCgpKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
