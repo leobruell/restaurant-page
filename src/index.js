51d340c7bb8977c787d9919f33541895cd5888c3
Leo Bruell <leobruell@gmail.com>
2022-12-07 14:25:47 -0500

    Set up project files

---
+++ src/index.js
@@ -0,0 +1,18 @@
+console.log('testifsndal sd')
+// function component() {
+//     const element = document.createElement('div');
+  
+//     const btn = document.createElement('button');
+  
+  
+//     element.innerHTML = "'Hello', 'webpack'"
+//     element.classList.add('hello');
+  
+//     btn.innerHTML = 'Click me and check the console!';
+  
+//     element.appendChild(btn);
+  
+//     return element;
+//   }
+  
+//   document.body.appendChild(component());
